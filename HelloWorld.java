9860f44c1beaae9805ae66ca8fbed38831b8b870
Aiden Dow <k1ard@yahoo.com>
2020-09-01 08:54:35 -0400

    Create HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/* 
+ * CIS 214, Fall 2020
+ * Author: Aiden Dow
+ * Date: 9/1/2020
+ * Assignment: Hello, World
+ * Comments:
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
